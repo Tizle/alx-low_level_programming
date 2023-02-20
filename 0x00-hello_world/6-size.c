e88dd46bb94c5d3bd4348ec231e07cf5eb7a1c01
Tizle <tizlelebona@gmail.com>
2023-02-20 05:01:44 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,5 +16,7 @@ int main(void)
 	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("size of a long long int: %lu byte(s)", (unsigned long)sizeof(d));
 	printf("size of a float int: %lu byte(s)", (unsigned long)sizeof(f));
+	printf("gcc 6-size.c -m32 -o size32 2> /tmp/32")
+	pritf("gcc 6-size.c -m64 -o size64 2> /tmp/64")
 	return (0);
 }
