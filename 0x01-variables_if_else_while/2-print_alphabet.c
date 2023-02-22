fb050634aac20932f099ac55b109c5bda7914d23
Tizle <tizlelebona@gmail.com>
2023-02-22 04:58:52 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Peints alphbets in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+
+}
