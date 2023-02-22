3029e67a2e9d8609ffd366317bd51cb14499d8bc
Tizle <tizlelebona@gmail.com>
2023-02-22 04:33:31 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,30 +1,29 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio>
 
 /**
- * main - ptintd if the number is positive, negetive or zero
- * Return:Always 0
+ * main - Determines if a number is positive, zero or negetive
+ *
  */
 int main(void)
 {
 	int n;
 
-	srand(time(0);
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
 	{
-	printf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-	printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	else
-	printf("%d is negetive\n, n);
+	{
+		printf("%d is negetive\n", n);
 	}
-
 	return (0);
-}
 
+}
