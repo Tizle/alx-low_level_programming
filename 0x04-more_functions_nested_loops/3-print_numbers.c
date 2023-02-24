413e01d6b1f89ce3ac576933bfaaba25de088bfe
Tizle <tizlelebona@gmail.com>
2023-02-24 14:06:39 -0800

    fizz buzz

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9
- * Return: void
+ * print_numbers - print 0-9
+ * return: void
  */
 
 void print_numbers(void)
@@ -11,7 +11,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(0);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
