4bb113d3eb5c8ca3bc733ca53a514d6dee0b4191
Tizle <tizlelebona@gmail.com>
2023-03-09 08:43:26 -0800

    Reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,11 +5,11 @@
  * @s: The string to be printed
  * Return: 0
  */
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
-	if (s[0] != '\0')
+	if (*s)
 	{
 		_print_rev_recursion(s + 1);
-		_putchar(s[0]);
+		_putchar(*s);
 	}
 }
