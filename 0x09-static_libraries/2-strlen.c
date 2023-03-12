a66bc7de6e86a9ccc733429650106932b48c49d3
Tizle <tizlelebona@gmail.com>
2023-03-12 12:58:08 -0700

    library

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -8,53 +8,11 @@ int _strlen(char *s)
 {
 	int longi = 0;
 
-
 	while (*s != '\0')
 	{
 		longi++;
 		s++;
 	}
 
-
 	return (longi);
-}
-
-
-
-
-
-
-============================
- 
-2-strncpy.c
-
-
-#include "main.h"
-/**
- * _strncpy - copy a string
- * @dest: input value
- * @src: input value
- * @n: input value
- *
- * Return: dest
- */
-char *_strncpy(char *dest, char *src, int n)
-{
-	int j;
-
-
-	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-		dest[j] = src[j];
-		j++;
-	}
-	while (j < n)
-	{
-		dest[j] = '\0';
-		j++;
-	}
-
-
-	return (dest);
 }
